11d9f0fc01ab2dc0105049c3c874f9cda1ce7211
TeenGud <blaineighteen@gmail.com>
2024-08-29 02:24:20 +1000

    eslint added

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,6 +1,7 @@
 import { lazy } from 'react';
 
 export const AboutPageAsync = lazy(() => new Promise(resolve => {
-    // @ts-ignore
+
+    // @ts-expect-error I'll fix this
     setTimeout(() => resolve(import('./AboutPage')), 1500)
 }) )
